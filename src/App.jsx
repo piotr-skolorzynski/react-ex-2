2eb55f591ed54c765d2fc5ac44e41bfd4ef72de6
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-09-25 21:21:26 +0200

    Handle two way binfing in UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
-function App() {
-  return <Header />;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <UserInput />
+    </>
+  );
+};
 
 export default App;
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,25 +1,71 @@
+import { useState } from "react";
+
 const UserInput = () => {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestemnt: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  };
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
-          <label htmlFor="">Initial Investment</label>
-          <input type="number" required />
+          <label htmlFor="initialInvestment">Initial Investment</label>
+          <input
+            type="number"
+            id="initialInvestment"
+            value={userInput.initialInvestment}
+            required
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
         </p>
         <p>
-          <label htmlFor="">Annual Investment</label>
-          <input type="number" required />
+          <label htmlFor="annualInvestemnt">Annual Investment</label>
+          <input
+            type="number"
+            id="annualInvestemnt"
+            value={userInput.annualInvestemnt}
+            required
+            onChange={(event) =>
+              handleChange("annualInvestemnt", event.target.value)
+            }
+          />
         </p>
       </div>
 
       <div className="input-group">
         <p>
-          <label htmlFor="">Expected Return</label>
-          <input type="number" required />
+          <label htmlFor="expectedReturn">Expected Return</label>
+          <input
+            type="number"
+            id="expectedReturn"
+            value={userInput.expectedReturn}
+            required
+            onChange={() => handleChange("expectedReturn", event.target.value)}
+          />
         </p>
         <p>
-          <label htmlFor="">Duration</label>
-          <input type="number" required />
+          <label htmlFor="duration">Duration</label>
+          <input
+            type="number"
+            id="duration"
+            value={userInput.duration}
+            required
+            onChange={() => handleChange("duration", event.target.value)}
+          />
         </p>
       </div>
     </section>

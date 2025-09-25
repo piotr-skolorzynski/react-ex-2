2b6d50ad9d5edb1112237607a97d82278c9fe62b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-09-25 22:04:41 +0200

    Output results in a list

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,42 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 const Results = ({ input }) => {
   const resultsData = calculateInvestmentResults(input);
-  return <div>Results...</div>;
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+
+  return (
+    <table id="result">
+      <tr>
+        <th>Year</th>
+        <th>Investment Value</th>
+        <th>Interest (Year)</th>
+        <th>Total Interest</th>
+        <th>Invested Capital</th>
+      </tr>
+      <tbody>
+        {resultsData.map((yearData) => {
+          const { year, interest, annualInvestment, valueEndOfYear } = yearData;
+          const totalInterest =
+            valueEndOfYear - annualInvestment * year - initialInvestment;
+
+          const totalAmountInvested = valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={year}>
+              <td>{year}</td>
+              <td>{formatter.format(valueEndOfYear)}</td>
+              <td>{formatter.format(interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default Results;

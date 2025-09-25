b266a6b0341e1632004698dad87775da6b3e656e
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-09-25 21:01:41 +0200

    Create UserInput component

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,29 @@
+const UserInput = () => {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Initial Investment</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label htmlFor="">Annual Investment</label>
+          <input type="number" required />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label htmlFor="">Duration</label>
+          <input type="number" required />
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default UserInput;
